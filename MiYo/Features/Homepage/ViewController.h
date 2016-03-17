bbbfbbcdeb66cde58583efd06cf1a1f96e68d168
项小盆友 <1259653875@qq.com>
2016-03-17 13:01:39 +0800

    MiYo

---
+++ MiYo/Features/Homepage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiYo
+//
+//  Created by 项小盆友 on 16/3/16.
+//  Copyright © 2016年 项小盆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiYoTests/MiYoTests.m
@@ -0,0 +1,39 @@
+//
+//  MiYoTests.m
+//  MiYoTests
+//
+//  Created by 项小盆友 on 16/3/16.
+//  Copyright © 2016年 项小盆友. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiYoTests : XCTestCase
+
+@end
+
+@implementation MiYoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
